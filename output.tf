428badbee56a2b6774b632e3a074880470f9b515
IanHF <ian.hernandez.f@gmail.com>
2025-01-01 19:19:38 -0300

    corregir pipeline

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
-# output "name" {
-#  value = module.ecr.repository_name
-# } 
+# output "ecr_repository_url" {
+#   value = aws_ecr_repository.sitioweb-static.repository_url
+#   description = "The URL of the ECR repository"
+# }
