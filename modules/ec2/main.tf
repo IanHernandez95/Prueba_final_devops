989454ee92b4c6befee1040cb9cf88b2f4af2689
IanHF <ian.hernandez.f@gmail.com>
2025-01-03 16:34:20 -0300

    Instalar el agente posteriormente

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -10,25 +10,7 @@ module "ec2" {
   vpc_security_group_ids = var.security_group_ids
   key_name = var.key_name
   associate_public_ip_address = true
-
-  user_data =  <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y wget unzip
-              wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-              dpkg -i amazon-cloudwatch-agent.deb
-              
-              cat <<EOT > /opt/aws/amazon-cloudwatch-agent/bin/config.json
-              {
-                "metrics": {
-                  "append_dimensions": {
-                    "InstanceId": "$${aws:InstanceId}"
-                  }
-                }
-              }
-              EOT
-              EOF
-
+  
   tags = {
     Terraform = "true"
     Environment = "dev"
