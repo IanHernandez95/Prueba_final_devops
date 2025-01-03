09997706a5531403cf427ab38cbac8e4cec39698
IanHF <ian.hernandez.f@gmail.com>
2025-01-03 12:35:17 -0300

    Vuelta a la idea original-8-cambio en el app.js

--- api/src/app.js
+++ api/src/app.js
@@ -16,9 +16,8 @@ app.get('/api', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
 
 module.exports = app;
